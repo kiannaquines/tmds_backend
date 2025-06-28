50d1b7c5900ed952e0813af1b6ee42a1bbfc2a00
kiannaquines <kjgnaquines@gmail.com>
2025-06-28 20:58:19 +0800

    added more configuration of the api backend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,16 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Str;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Gate;
+
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+
+use App\Models\User;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +29,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Scramble::configure()
+            ->routes(function (Route $route) {
+                return Str::startsWith($route->uri, 'api/');
+            });
+
+        Gate::define('viewApiDocs', function (User $user) {
+            return in_array($user->email, ['angelo@gmail.com']);
+        });
+
+        Scramble::configure()
+        ->withDocumentTransformers(function (OpenApi $openApi) {
+            $openApi->secure(
+                SecurityScheme::http('bearer')
+            );
+        });
     }
 }
