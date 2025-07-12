06f0a6678f13f2d7ba15847062426bb3b0969634
kiannaquines <kjgnaquines@gmail.com>
2025-07-12 23:50:13 +0800

    Change default authentication guard from 'api' to 'web' in auth configuration

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'api'),
+        'guard' => env('AUTH_GUARD', 'web'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -36,7 +36,7 @@ return [
     */
 
     'guards' => [
-        'api' => [
+        'web' => [
             'driver' => 'session',
             'provider' => 'users',
         ],
